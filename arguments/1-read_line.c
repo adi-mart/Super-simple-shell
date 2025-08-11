5a57c8ffb951195d9ed0eb0e962c297668f15cdd
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-11 14:21:19 +0200

    add: implement command reading program with error handling

--- arguments/1-read_line.c
+++ arguments/1-read_line.c
@@ -1,4 +1,32 @@
-/* Write a program that prints "$ ", wait for the user to enter a command, prints it on the next line */
-/* man 3 getline */
-/* important make sure you read the man, and the RETURN VALUE section */
-/* #advanced: Write your own version of getline */
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - reads a line from standard input
+ * @ac: argument count
+ * @av: argument vector
+ * Return: Always 0
+ */
+int main(int ac, char **av)
+
+{
+	char *line = NULL;  // Pointeur pour stocker la ligne saisie
+	size_t len = 0;     // Taille du tampon
+	ssize_t nread;      // Nombre de caractères lus
+
+	while (1) // Boucle infinie
+	{ 
+		printf("$ ");  // Affiche le prompt
+		nread = getline(&line, &len, stdin);  // Lit l'entrée de l'utilisateur
+
+		if (nread == -1) {  // Vérifie la fin de fichier ou une erreur
+			perror("getline"); // Affiche l'erreur
+			break; // Sort de la boucle
+		}
+
+	printf("%s", line);  // Affiche la commande entrée
+	} // Fin de la boucle
+
+	free(line);  // Libère la mémoire allouée
+	return 0; // Retourne 0 pour indiquer le succès
+}

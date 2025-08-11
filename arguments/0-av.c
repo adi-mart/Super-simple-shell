e02084cdb32de95e267a6631afc9f4aecbfccb93
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-11 11:05:15 +0200

    add: implement argument printing program without using argument count

--- arguments/0-av.c
+++ arguments/0-av.c
@@ -1,1 +1,24 @@
-/* Write a program that prints all the arguments, without using ac */
+#include <stdio.h>
+/**
+ * main - prints the arguments passed to the program
+ * @ac: argument count
+ * @av: argument vector
+ * Return: Always 0
+ */
+
+int main(int ac, char **av)
+{
+	char **ptr;
+	int i;
+
+	ptr = av;
+	i = 0;
+
+	while (*ptr != NULL)
+	{
+		printf("av[%d] is : %s\n", i, *ptr);
+		ptr++;
+		i++;
+	}
+	return (0);
+}

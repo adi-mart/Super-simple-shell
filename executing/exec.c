38703dc0fa7ab650401392d0260f505b6c1252d5
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-11 15:08:45 +0200

    refactor: clean up execve example code and improve comments

--- executing/exec.c
+++ executing/exec.c
@@ -1,3 +1,20 @@
-/* execve example */
-/* The system call execve allows a process to execute another program */
-/* Warning: don't forget to add the environment in your Shell! */
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - execve example
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+
+    printf("Before execve\n");
+    if (execve(argv[0], argv, NULL) == -1)
+    {
+        perror("Error:");
+    }
+    printf("After execve\n");
+    return (0);
+}

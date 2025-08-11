99d3625ae235c64c6165cc1fcf8f9242f9d74e65
Krapaud <mur.mickael@gmail.com>
2025-08-11 14:52:41 +0200

    add: implement string splitting functionality to extract words from a sentence

--- arguments/2-command_line_to_av.c
+++ arguments/2-command_line_to_av.c
@@ -1,3 +1,26 @@
-/* Write a function that splits a string and returns an array of each word of the string */
-/* man strtok */
-/* #advanced: Write the function without strtok */
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char string[100] = "Cet exercice est incompréhensible aidez moi";
+    char* words[10];
+    size_t count = 0;
+
+    char* token = strtok(string, " ");
+    while (token != NULL)
+    { 
+        words[count] = token;
+        count++;
+
+        token = strtok(NULL, " ");
+    }
+
+    printf("Found: %zu words. They are:\n", count);
+    for (size_t i = 0; i < count; i++)
+    {
+        puts(words[i]);
+    }
+
+    return 0;
+}

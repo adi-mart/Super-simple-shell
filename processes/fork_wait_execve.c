049a1ff8d4b5e9783c3fa129c5dc1da7a0d2a66d
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-08-11 16:13:06 +0200

    refactor: restructure fork example for clarity and improved functionality

--- processes/fork_wait_execve.c
+++ processes/fork_wait_execve.c
@@ -1,20 +1,34 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-/**
- * main - execve example
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+    pid_t child_pid;
+    int i, status;
+    char *argv[] = {"/bin/ls", "-l", "/tmp/", NULL};
 
-    printf("Before execve\n");
-    if (execve(argv[0], argv, NULL) == -1)
+    for (i = 0; i < 5; i++)
     {
-        perror("Error:");
+        child_pid = fork();
+        if (child_pid == -1)
+        {
+            perror("fork");
+            return (1);
+        }
+
+        if (child_pid == 0)
+        {
+            printf("Before execve\n");
+            if (execve(argv[0], argv, NULL) == -1)
+            {
+                perror("Error:");
+            }
+            printf("After execve\n");;
+        }
+        else
+        {
+            wait(&status);
+        }
     }
-    printf("After execve\n");
     return (0);
 }

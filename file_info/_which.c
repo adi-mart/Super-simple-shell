d97715263e849f998c7c9c1d0fd6981ae519a360
Krapaud <mur.mickael@gmail.com>
2025-08-12 15:33:19 +0200

    refactor: enhance _which.c with improved structure and usage instructions

--- file_info/_which.c
+++ file_info/_which.c
@@ -1,3 +1,45 @@
-/* Exercise: find a file in the PATH */
-/* Write a program that looks for files in the current PATH */
-/* Usage: _which filename ... */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+	char full_path[1024];
+	char *dir;
+	char *path;
+	char *path_copy;
+	int i;
+
+    if (ac < 2)
+    {
+        fprintf(stderr, "Usage: %s filename ...\n", av[0]);
+        return 1;
+    }
+
+    path = getenv("PATH");
+    if (!path)
+        return 1;
+
+    for (i = 1; i < ac; i++)
+    {
+        path_copy = strdup(path);
+        if (!path_copy)
+            continue;
+
+        dir = strtok(path_copy, ":");
+        while (dir)
+        {
+            snprintf(full_path, sizeof(full_path), "%s/%s", dir, av[i]);
+            if (access(full_path, X_OK) == 0)
+            {
+                printf("%s\n", full_path);
+                break;
+            }
+            dir = strtok(NULL, ":");
+        }
+        free(path_copy);
+    }
+    return 0;
+}
+

c0599b0d03a3a3749b5590914f9b8fa1f0753b94
Krapaud <mur.mickael@gmail.com>
2025-08-14 10:13:52 +0200

    feat: implement function to print directories in PATH environment variable

--- environment/3-path.c
+++ environment/3-path.c
@@ -1,1 +1,23 @@
-/* Write a function that prints each directory contained in the environment variable PATH, one directory per line */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * get_location - Get the location of a command in the PATH
+ * @command: The command to find
+ *
+ * Return: The full path to the command if found, NULL otherwise
+ */
+int main(void)
+{
+    char *path = getenv("PATH");
+    char *path_copy = strdup(path);
+    char *dir = strtok(path_copy, ":");
+    while (dir)
+    {
+        printf("%s\n", dir);
+        dir = strtok(NULL, ":");
+    }
+    free(path_copy);
+    return 0;
+}
